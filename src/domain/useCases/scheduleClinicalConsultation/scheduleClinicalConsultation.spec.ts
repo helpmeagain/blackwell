e06d8caf0285e6a4b8eaba68f943f68c81f1ba57
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-10 19:16:58 -0300

    test(unit): add repository to clinical consultation test
    Also add new commands for vitest

--- src/domain/useCases/scheduleClinicalConsultation/scheduleClinicalConsultation.spec.ts
+++ src/domain/useCases/scheduleClinicalConsultation/scheduleClinicalConsultation.spec.ts
@@ -1,9 +1,17 @@
 import { expect, test } from 'vitest';
 import scheduleClinicalConsultation from './scheduleClinicalConsultation';
+import ClinicalConsultationRepository from '../../repositories/ClinicalConsultationRepository';
+import ClinicalConsultation from '../../entities/clinicalConsultation';
 
-test('Create a Clinical Consultation', () => {
-    const appointment = new scheduleClinicalConsultation()
-    const response = appointment.execute({
+const fakeClinicalConsultationRepository: ClinicalConsultationRepository = {
+    create: async (clinicalConsultation: ClinicalConsultation) => {
+        return;
+    }
+}
+
+test('Create a Clinical Consultation', async () => {
+    const appointment = new scheduleClinicalConsultation(fakeClinicalConsultationRepository)
+    const response = await appointment.execute({
         clinicianId: '1',
         patientId: '1',
         appointmentDate: new Date(Date.UTC(2021, 0, 1, 0, 0, 0))
