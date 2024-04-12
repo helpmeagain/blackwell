520cbfb77fd72efb2d07dce8c8d0fbd31a36cccb
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-12 20:11:51 -0300

    test(unit): add test to 'delete clinical consultation' use case

---
+++ src/application/use-cases/delete-clinical-consultation-by-id/delete-clinical-consultation-by-id.spec.ts
@@ -0,0 +1,44 @@
+import { DeleteClinicalConsultationById } from './delete-clinical-consultation-by-id';
+import { InMemoryConsultationRepository } from 'test/repositories/in-memory-consultation-repository';
+import { makeConsultation } from 'test/factories/make-consultation';
+import { UniqueEntityId } from '@/domain/value-objects/unique-entity-id/unique-entity-id';
+
+let inMemoryConsultationRepository: InMemoryConsultationRepository;
+let sut: DeleteClinicalConsultationById;
+
+describe('Delete a clinical consultation By Id', () => {
+  beforeEach(() => {
+    inMemoryConsultationRepository = new InMemoryConsultationRepository();
+    sut = new DeleteClinicalConsultationById(inMemoryConsultationRepository);
+  });
+
+  it('should be able to delete a clinical consultation by id', async () => {
+    const newClinicalConsultation = makeConsultation(
+      { clinicianId: new UniqueEntityId('clinicianId-1') },
+      new UniqueEntityId('consultationId-1'),
+    );
+    await inMemoryConsultationRepository.create(newClinicalConsultation);
+
+    await sut.execute({
+      clinicalConsultationId: 'consultationId-1',
+      clinicianId: 'clinicianId-1',
+    });
+
+    expect(inMemoryConsultationRepository.items).toHaveLength(0);
+  });
+
+  it('should not be able to delete a clinical consultation from another user', async () => {
+    const newClinicalConsultation = makeConsultation(
+      { clinicianId: new UniqueEntityId('clinicianId-1') },
+      new UniqueEntityId('consultationId-1'),
+    );
+    await inMemoryConsultationRepository.create(newClinicalConsultation);
+
+    expect(() => {
+      return sut.execute({
+        clinicalConsultationId: 'consultationId-1',
+        clinicianId: 'clinicianId-2',
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
