76de0f30adc9768c581e958b89f1fad8d2b62d07
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-15 19:53:57 -0300

    feat(domain): update patient entity
    Add new props.
    Add create method.
    Add getters and setters.

--- src/domain/entities/patient.ts
+++ src/domain/entities/patient.ts
@@ -1,14 +1,69 @@
-import { BaseEntity } from '@domain/common/base-entity';
 import { UniqueEntityId } from '@domain/value-objects/unique-entity-id/unique-entity-id';
+import { AggregateRoot } from '../common/aggregate-root';
+import { Optional } from '../common/optional-type';
 
 interface PatientProps {
   name: string;
   surname: string;
+  birthDate: Date;
+  phoneNumber: string;
+  email: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-export class Patient extends BaseEntity<PatientProps> {
-  static create(props: PatientProps, id?: UniqueEntityId) {
-    const patient = new Patient(props, id);
+export class Patient extends AggregateRoot<PatientProps> {
+  static create(props: Optional<PatientProps, 'createdAt'>, id?: UniqueEntityId) {
+    const patient = new Patient(
+      { ...props, createdAt: props.createdAt ?? new Date() },
+      id,
+    );
     return patient;
+  }
+
+  // Getters //
+  get name(): string {
+    return this.props.name;
+  }
+
+  get surname(): string {
+    return this.props.surname;
+  }
+
+  get birthDate(): Date {
+    return this.props.birthDate;
+  }
+
+  get phoneNumber(): string {
+    return this.props.phoneNumber;
+  }
+
+  get email(): string {
+    return this.props.email;
+  }
+
+  // Setter //
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+    this.touch();
+  }
+
+  set surname(surname: string) {
+    this.props.surname = surname;
+    this.touch();
+  }
+
+  set phoneNumber(phoneNumber: string) {
+    this.props.phoneNumber = phoneNumber;
+    this.touch();
+  }
+
+  set email(email: string) {
+    this.props.email = email;
+    this.touch();
   }
 }
