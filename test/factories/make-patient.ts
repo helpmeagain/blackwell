d21ee09eb536da341d31057c312c0911580e270d
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-17 14:28:20 -0300

    test(unit): add test to 'delete patient by id' use case

--- test/factories/make-patient.ts
+++ test/factories/make-patient.ts
@@ -1,7 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { Patient, PatientProps } from '@entities/patient';
 import { UniqueEntityId } from '@domain/value-objects/unique-entity-id/unique-entity-id';
-import { MedicalRecord } from '@/domain/entities/medical-record';
 
 export function makePatient(override: Partial<PatientProps> = {}, id?: UniqueEntityId) {
   const mockGender = faker.person.sexType();
@@ -21,13 +20,5 @@ export function makePatient(override: Partial<PatientProps> = {}, id?: UniqueEnt
     id,
   );
 
-  const medicalRecord = MedicalRecord.create({
-    patientId: patient.id,
-    consultationId: [],
-    diagnosis: '',
-    comorbidity: '',
-  });
-
-  patient.medicalRecord = medicalRecord;
   return patient;
 }
