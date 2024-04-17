39bca3663bd6f12204ac40bb2f03fde3d8a2f3b2
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-17 11:25:15 -0300

    test: add 'make patient' factory

---
+++ test/factories/make-patient.ts
@@ -0,0 +1,33 @@
+import { faker } from '@faker-js/faker';
+import { Patient, PatientProps } from '@entities/patient';
+import { UniqueEntityId } from '@domain/value-objects/unique-entity-id/unique-entity-id';
+import { MedicalRecord } from '@/domain/entities/medical-record';
+
+export function makePatient(override: Partial<PatientProps> = {}, id?: UniqueEntityId) {
+  const mockGender = faker.person.sexType();
+  const mockName = faker.person.firstName(mockGender);
+  const mockSurName = faker.person.lastName(mockGender);
+
+  const patient = Patient.create(
+    {
+      name: mockName,
+      surname: mockSurName,
+      gender: mockGender,
+      birthDate: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }),
+      phoneNumber: faker.phone.number(),
+      email: faker.internet.email({ firstName: mockName, lastName: mockSurName }),
+      ...override,
+    },
+    id,
+  );
+
+  const medicalRecord = MedicalRecord.create({
+    patientId: patient.id,
+    consultationId: [],
+    diagnosis: '',
+    comorbidity: '',
+  });
+
+  patient.medicalRecord = medicalRecord;
+  return patient;
+}
