481eebcc9a61a32d05db0f619aa83c7352afc2ea
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-19 14:17:03 -0300

    feat(app): update test to 'delete consultation by id' use case

--- src/application/use-cases/consultation/delete-consultation-by-id/delete-consultation-by-id-use-case.spec.ts
+++ src/application/use-cases/consultation/delete-consultation-by-id/delete-consultation-by-id-use-case.spec.ts
@@ -3,41 +3,59 @@ import { InMemoryConsultationRepository } from 'test/repositories/in-memory-cons
 import { makeConsultation } from 'test/factories/make-consultation';
 import { UniqueEntityId } from '@domain/value-objects/unique-entity-id/unique-entity-id';
 import { NotAllowed } from '@/application/common/error-handler/errors/not-allowed';
+import { InMemoryPatientRepository } from 'test/repositories/in-memory-patient-repository';
+import { makePatient } from 'test/factories/make-patient';
 
-let inMemoryRepository: InMemoryConsultationRepository;
+let inMemoryConsultationRepository: InMemoryConsultationRepository;
+let inMemoryPatientRepository: InMemoryPatientRepository;
 let sut: DeleteConsultationByIdUseCase;
 
 describe('Delete a consultation By Id', () => {
   beforeEach(() => {
-    inMemoryRepository = new InMemoryConsultationRepository();
-    sut = new DeleteConsultationByIdUseCase(inMemoryRepository);
+    inMemoryConsultationRepository = new InMemoryConsultationRepository();
+    inMemoryPatientRepository = new InMemoryPatientRepository();
+    sut = new DeleteConsultationByIdUseCase(
+      inMemoryConsultationRepository,
+      inMemoryPatientRepository,
+    );
   });
 
   it('should be able to delete a consultation by id', async () => {
-    const newConsultation = makeConsultation(
-      { clinicianId: new UniqueEntityId('clinicianId-1') },
-      new UniqueEntityId('consultationId-1'),
-    );
-    await inMemoryRepository.create(newConsultation);
+    const newPatient = makePatient();
+    const newConsultation = makeConsultation({
+      patientId: newPatient.id,
+      clinicianId: new UniqueEntityId('clinicianId-1'),
+    });
+    await inMemoryPatientRepository.create(newPatient);
+    await inMemoryConsultationRepository.create(newConsultation);
 
     const result = await sut.execute({
-      consultationId: 'consultationId-1',
+      consultationId: newConsultation.id.toString(),
+      patientId: newPatient.id.toString(),
       clinicianId: 'clinicianId-1',
     });
 
     expect(result.isRight()).toBe(true);
-    expect(inMemoryRepository.items).toHaveLength(0);
+    expect(inMemoryConsultationRepository.items).toHaveLength(0);
+    if (result.isRight()) {
+      expect(newPatient.medicalRecord.consultationsIds.getRemovedItems()).toEqual([
+        newConsultation.id,
+      ]);
+    }
   });
 
   it('should not be able to delete a consultation from another user', async () => {
-    const newConsultation = makeConsultation(
-      { clinicianId: new UniqueEntityId('clinicianId-1') },
-      new UniqueEntityId('consultationId-1'),
-    );
-    await inMemoryRepository.create(newConsultation);
+    const newPatient = makePatient();
+    const newConsultation = makeConsultation({
+      patientId: newPatient.id,
+      clinicianId: new UniqueEntityId('clinicianId-1'),
+    });
+    await inMemoryPatientRepository.create(newPatient);
+    await inMemoryConsultationRepository.create(newConsultation);
 
     const result = await sut.execute({
-      consultationId: 'consultationId-1',
+      consultationId: newConsultation.id.toString(),
+      patientId: newPatient.id.toString(),
       clinicianId: 'clinicianId-2',
     });
 
