0c4c7bed510de3303b302eb792c51c15f15429ac
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-20 15:40:21 -0300

    feat(app): remove unused code

--- src/application/repositories/medical-record-repository.ts
+++
@@ -1,8 +0,0 @@
-import { MedicalRecord } from '@entities/medical-record';
-
-export interface MedicalRecordRepository {
-  findById: (id: string) => Promise<MedicalRecord | null>;
-  create: (medicalRecord: MedicalRecord) => Promise<void>;
-  save: (medicalRecord: MedicalRecord) => Promise<void>;
-  delete: (medicalRecord: MedicalRecord) => Promise<void>;
-}
