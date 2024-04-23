f740307ba5ece2ea611f84b912fb5750da0690f4
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-23 18:41:39 -0300

    feat(app): update 'on consultation created' event

--- src/application/subscribers/on-consultation-created/on-consultation-created.ts
+++ src/application/subscribers/on-consultation-created/on-consultation-created.ts
@@ -1,9 +1,14 @@
+import { PatientRepository } from '@/application/repositories/patient-repository';
+import { CreateNotificationUseCase } from '@/application/use-cases/notification/create-notification/create-notification-use-case';
 import { DomainEvents } from '@/domain/common/events/domain-events';
 import { EventHandler } from '@/domain/common/events/event-handler';
 import { ConsultationCreatedEvent } from '@/domain/events/consultation-created-event';
 
 export class OnConsultationCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private readonly patientRepository: PatientRepository,
+    private readonly createNotification: CreateNotificationUseCase,
+  ) {
     this.setupSubscriptions();
   }
 
@@ -15,6 +20,16 @@ export class OnConsultationCreated implements EventHandler {
   }
 
   private async sendNewNotification({ consultation }: ConsultationCreatedEvent) {
-    console.log(consultation);
+    const patient = await this.patientRepository.findById(
+      consultation.patientId.toString(),
+    );
+
+    if (patient) {
+      await this.createNotification.execute({
+        recipientId: patient.id.toString(),
+        title: `New consultation has been created!`,
+        message: `The consultation is scheduled for ${consultation.appointmentDate.toLocaleString()}.`,
+      });
+    }
   }
 }
