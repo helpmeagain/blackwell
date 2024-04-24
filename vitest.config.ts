e4502bc788d553ee97b332a3c4f0d854e6a16d5a
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-24 12:00:05 -0300

    build: update vitest and add 'swc' to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
+import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
   test: {
     globals: true,
   },
