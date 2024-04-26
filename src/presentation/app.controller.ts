fada0eaf6c3d56b1d577aa62d84ef4b65824e5f9
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-26 14:53:13 -0300

    feat(infra): add prisma client

--- src/presentation/app.controller.ts
+++ src/presentation/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from '@/infrastructure/persistence/prisma/prisma.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/clinician')
+  async getClinician() {
+    return await this.prismaService.clinician.findMany();
   }
 }
