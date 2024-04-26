f55338be9d6639461e3746e48a54bdce76336486
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-26 17:30:15 -0300

    feat(pres): add pipe validation with zod

--- src/presentation/controllers/clinician/create-clinician.controller.ts
+++ src/presentation/controllers/clinician/create-clinician.controller.ts
@@ -1,15 +1,38 @@
 import { PrismaService } from '@/infrastructure/persistence/prisma/prisma.service';
-import { Body, ConflictException, Controller, HttpCode, Post } from '@nestjs/common';
+import { ZodValidationPipe } from '@/presentation/pipes/zod-validation-pipe';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { hash } from 'bcryptjs';
+import { z } from 'zod';
+
+const createClinicianSchema = z.object({
+  name: z.string(),
+  surname: z.string(),
+  slug: z.string(),
+  gender: z.string(),
+  occupation: z.string(),
+  phoneNumber: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateClinicianSchema = z.infer<typeof createClinicianSchema>;
 
 @Controller('clinicians')
 export class CreateClinicianController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
+  @UsePipes(new ZodValidationPipe(createClinicianSchema))
   @HttpCode(201)
   @HttpCode(409)
-  async handle(@Body() body: any) {
+  async handle(@Body() body: CreateClinicianSchema) {
     const { name, surname, slug, gender, occupation, phoneNumber, email, password } =
       body;
 
