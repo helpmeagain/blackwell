c3e8766721fc0a02919aceebe0ea2d7d01c6704e
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-28 19:27:56 -0300

    feat(infra): add 'current user' decorator

--- src/presentation/controllers/consultation/create-consultation.controller.ts
+++ src/presentation/controllers/consultation/create-consultation.controller.ts
@@ -1,14 +1,19 @@
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '@/infrastructure/auth/jwt-auth.guard';
+import { CurrentUser } from '@/infrastructure/auth/current-user-decorator';
+import { UserPayload } from '@/infrastructure/auth/jwt.strategy';
 
 @Controller('consultations')
 @UseGuards(JwtAuthGuard)
 export class CreateConsultationController {
+  constructor() {}
+
   @Post()
   @ApiBearerAuth()
   @ApiTags('Consultations')
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user);
     return 'ok';
   }
 }
