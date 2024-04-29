8ad4e1bb093970b1efc871df91476f4ba8f73c0e
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-29 17:53:37 -0300

    test(e2e): add test to 'authenticate clinician' controller

---
+++ src/presentation/controllers/clinician/authenticate-clinician.controller.e2e-spec.ts
@@ -0,0 +1,48 @@
+import { PrismaService } from '@/infrastructure/persistence/prisma/prisma.service';
+import { AppModule } from '@/presentation/app.module';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { hash } from 'bcryptjs';
+import request from 'supertest';
+
+describe('Authenticate clinician [E2E]', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+    await app.init();
+  });
+
+  test('[POST] /clinicians', async () => {
+    await prisma.clinician.create({
+      data: {
+        name: 'John',
+        surname: 'Doe',
+        slug: 'john-doe',
+        gender: 'male',
+        occupation: 'doctor',
+        phoneNumber: '123456789',
+        email: 'jonhdoe@email.com',
+        password: await hash('12345', 8),
+      },
+    });
+
+    const result = await request(app.getHttpServer())
+      .post('/clinicians/authenticate')
+      .send({
+        email: 'jonhdoe@email.com',
+        password: '12345',
+      });
+
+    expect(result.statusCode).toBe(201);
+    expect(result.body).toEqual({
+      access_token: expect.any(String),
+    });
+  });
+});
