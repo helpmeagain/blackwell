b94b38f45cc8d3fe3a050a4ba0c7fb1a133de5c3
Felipe Marques <felipe.comarques1208@gmail.com>
2024-05-03 15:06:12 -0300

    fix: change repositories from interfaces to abstract classes
    This change is needed for NestJS compile without errors to JS.

--- src/application/repositories/patient-repository.ts
+++ src/application/repositories/patient-repository.ts
@@ -1,12 +1,15 @@
 import { MedicalRecord } from '@/domain/entities/medical-record';
 import { Patient } from '@entities/patient';
 
-export interface PatientRepository {
-  findBySlug: (slug: string) => Promise<Patient | null>;
-  findById: (id: string) => Promise<Patient | null>;
-  findMedicalRecordById: (medicalRecordId: string) => Promise<MedicalRecord | null>;
-  create: (patient: Patient) => Promise<void>;
-  save: (patient: Patient) => Promise<void>;
-  saveMedicalRecord: (medicalRecord: MedicalRecord) => Promise<void>;
-  delete: (consultation: Patient) => Promise<void>;
+export abstract class PatientRepository {
+  abstract findBySlug: (slug: string) => Promise<Patient | null>;
+  abstract findById: (id: string) => Promise<Patient | null>;
+  abstract findMedicalRecordById: (
+    medicalRecordId: string,
+  ) => Promise<MedicalRecord | null>;
+
+  abstract create: (patient: Patient) => Promise<void>;
+  abstract save: (patient: Patient) => Promise<void>;
+  abstract saveMedicalRecord: (medicalRecord: MedicalRecord) => Promise<void>;
+  abstract delete: (consultation: Patient) => Promise<void>;
 }
