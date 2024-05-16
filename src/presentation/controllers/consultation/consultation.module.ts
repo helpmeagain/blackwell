82c62dd60053e0c3819b7ba829110653be6207b6
Felipe Marques <felipe.comarques1208@gmail.com>
2024-05-15 22:27:52 -0300

    feat(pres): add 'get consultation by id' controller

--- src/presentation/controllers/consultation/consultation.module.ts
+++ src/presentation/controllers/consultation/consultation.module.ts
@@ -5,10 +5,20 @@ import { CreateConsultationController } from './create-consultation/create-consu
 import { NestCreateConsultationUseCase } from '@/infrastructure/adapter/consultation/nest-create-consultation-use-case';
 import { DeleteConsultationController } from './delete-consultation/delete-consultation.controller';
 import { NestDeleteConsultationByIdUseCase } from '@/infrastructure/adapter/consultation/nest-delete-consultation-use-case';
+import { NestGetConsultationByIdUseCase } from '@/infrastructure/adapter/consultation/nest-get-consultation-by-id-use-case';
+import { GetByIdConsultationsController } from './get-consultation-by-id/get-consultation-by-id.controller';
 
 @Module({
   imports: [PersistenceModule, CryptographyModule],
-  controllers: [CreateConsultationController, DeleteConsultationController],
-  providers: [NestCreateConsultationUseCase, NestDeleteConsultationByIdUseCase],
+  controllers: [
+    CreateConsultationController,
+    DeleteConsultationController,
+    GetByIdConsultationsController,
+  ],
+  providers: [
+    NestCreateConsultationUseCase,
+    NestDeleteConsultationByIdUseCase,
+    NestGetConsultationByIdUseCase,
+  ],
 })
 export class ConsultationModule {}
