dd12d117f35e3e325c4378935a8dccc88618536b
Felipe Marques <felipe.comarques1208@gmail.com>
2024-10-05 20:43:19 -0300

    feat: add 'get neurofunctional record' controller
    Add 'get by id' and 'get by patient id'

--- src/presentation/controllers/specific-records/neurofunctional-record/neurofunctional-record.module.ts
+++ src/presentation/controllers/specific-records/neurofunctional-record/neurofunctional-record.module.ts
@@ -1,12 +1,23 @@
-// import { CryptographyModule } from '@/infrastructure/cryptography/cryptography.module';
 import { PersistenceModule } from '@/infrastructure/persistence/persistence.module';
 import { Module } from '@nestjs/common';
 import { CreateNeurofunctionalRecordController } from './create-record/create-record.controller';
 import { NestCreateNeurofunctionalRecordUseCase } from '@/infrastructure/adapter/specific-records/neurofunctional-record/nest-create-neurofunctional-record';
+import { GetByIdNeurofunctionalController } from './get-record-by-id/get-record-by-id.controller';
+import { NestGetNeurofunctionalByIdUseCase } from '@/infrastructure/adapter/specific-records/neurofunctional-record/nest-get-record-by-id';
+import { GetByPatientIdNeurofunctionalController } from './get-record-by-patient-id/get-record-by-patient-id.controller';
+import { NestGetNeurofunctionalByPatientIdUseCase } from '@/infrastructure/adapter/specific-records/neurofunctional-record/nest-get-record-by-patient-id';
 
 @Module({
   imports: [PersistenceModule],
-  controllers: [CreateNeurofunctionalRecordController],
-  providers: [NestCreateNeurofunctionalRecordUseCase],
+  controllers: [
+    CreateNeurofunctionalRecordController,
+    GetByIdNeurofunctionalController,
+    GetByPatientIdNeurofunctionalController,
+  ],
+  providers: [
+    NestCreateNeurofunctionalRecordUseCase,
+    NestGetNeurofunctionalByIdUseCase,
+    NestGetNeurofunctionalByPatientIdUseCase,
+  ],
 })
 export class NeurofunctionalRecordModule {}
