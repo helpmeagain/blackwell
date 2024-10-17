1d5479747e0d76c049ed4fd75b1933b78b21a7e5
Felipe Marques <felipe.comarques1208@gmail.com>
2024-10-17 16:30:09 -0300

    feat(app): improved 'get pending authorization users' use case

--- src/application/use-cases/specific-records/neurofunctional-record/authorization/get-pending-authorization/get-pending-authorization.ts
+++ src/application/use-cases/specific-records/neurofunctional-record/authorization/get-pending-authorization/get-pending-authorization.ts
@@ -2,6 +2,9 @@ import { Either, left, right } from '@error/either';
 import { ResourceNotFound } from '@error/errors/resource-not-found';
 import { NeurofunctionalRecordRepository } from '@/application/repositories/neurofunctional-record-repository';
 import { UnauthorizedUser } from '@/application/common/error-handler/errors/unauthorized';
+import { PatientRepository } from '@/application/repositories/patient-repository';
+import { ClinicianRepository } from '@/application/repositories/clinician-repository';
+import { GetPendingAuthorizationUsersResponse } from './get-pending-authorization-response';
 
 interface getPendingAuthorizationUsersRequest {
   id: string;
@@ -10,28 +13,62 @@ interface getPendingAuthorizationUsersRequest {
 
 type getPendingAuthorizationUsersResponse = Either<
   ResourceNotFound | UnauthorizedUser,
-  { pendingAuthorizationUsers: string[] }
+  { pendingAuthorizationUsers: GetPendingAuthorizationUsersResponse[] }
 >;
 
 export class GetPendingAuthorizationUsersUseCase {
-  constructor(private readonly repository: NeurofunctionalRecordRepository) {}
+  constructor(
+    private readonly recordRepository: NeurofunctionalRecordRepository,
+    private readonly patientRepository: PatientRepository,
+    private readonly clinicianRepository: ClinicianRepository,
+  ) {}
 
   async execute(
     req: getPendingAuthorizationUsersRequest,
   ): Promise<getPendingAuthorizationUsersResponse> {
     const { id, currentUserId } = req;
-    const neurofunctionalRecord = await this.repository.findById(id);
+    const neurofunctionalRecord = await this.recordRepository.findById(id);
 
     if (!neurofunctionalRecord) {
       return left(new ResourceNotFound());
     }
 
-    if (neurofunctionalRecord.clinicianId.toString() !== currentUserId) {
+    if (neurofunctionalRecord.patientId.toString() !== currentUserId) {
       return left(new UnauthorizedUser());
     }
 
-    const pendingAuthorizationUsers =
-      neurofunctionalRecord.pendingAuthorizationUsers ?? [];
+    const authorizationUsers = neurofunctionalRecord.pendingAuthorizationUsers ?? [];
+
+    const pendingAuthorizationUsers = await Promise.all(
+      authorizationUsers.map(async (userId) => {
+        const patient = await this.patientRepository.findById(userId);
+        if (patient) {
+          return {
+            userId,
+            name: patient.name,
+            surname: patient.surname,
+            role: 'Patient',
+          };
+        }
+
+        const clinician = await this.clinicianRepository.findById(userId);
+        if (clinician) {
+          return {
+            userId,
+            name: clinician.name,
+            surname: clinician.surname,
+            role: 'Clinician',
+          };
+        }
+
+        return {
+          userId,
+          name: 'Unknown',
+          surname: 'Unknown',
+          role: 'Unknown',
+        };
+      }),
+    );
 
     return right({ pendingAuthorizationUsers });
   }
